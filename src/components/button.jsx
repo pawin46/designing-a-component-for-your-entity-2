85c0079dea114a325afc77769b22733b2f367dea
pawin46 <pawinde@gmail.com>
2025-03-06 10:33:11 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+import React from 'react';
+
+export const ViewProductButton = () => {
+    return (
+        <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600 transition-all"
+        >
+            View Product
+        </button>
+    );
+};
